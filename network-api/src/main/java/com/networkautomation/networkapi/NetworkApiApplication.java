ff0705b297a92794b090c9d86e65137e4564b81e
Raj Pandya <rajpandya@Rajs-MacBook.local>
2026-09-11 21:27:57 -0400

    Initialize Spring Boot network API

---
+++ network-api/src/main/java/com/networkautomation/networkapi/NetworkApiApplication.java
@@ -0,0 +1,13 @@
+package com.networkautomation.networkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkApiApplication.class, args);
+	}
+
+}
---
+++ network-api/src/test/java/com/networkautomation/networkapi/NetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.networkautomation.networkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
