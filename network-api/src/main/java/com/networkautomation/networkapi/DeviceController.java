7cd4d85ac35e5ce38de93db3c941636806bdee14
Raj Pandya <rajpandya@Rajs-MacBook.local>
2026-09-11 21:56:40 -0400

    Return 404 when device is not found

--- network-api/src/main/java/com/networkautomation/networkapi/DeviceController.java
+++ network-api/src/main/java/com/networkautomation/networkapi/DeviceController.java
@@ -3,6 +3,7 @@ package com.networkautomation.networkapi;
 import com.networkautomation.networkapi.model.Device;
 import com.networkautomation.networkapi.service.DeviceService;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,14 @@ public class DeviceController {
     }
 
     @GetMapping("/{ip}")
-    public Device getDevice(@PathVariable String ip) {
-        return deviceService.getDeviceByIp(ip);
+    public ResponseEntity<Device> getDevice(@PathVariable String ip) {
+
+        Device device = deviceService.getDeviceByIp(ip);
+
+        if (device == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(device);
     }
 }
